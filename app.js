cdf3c31afdbf1b51b9f89969e159cb36e01d7206
hanh <hanh@gmail.com>
2023-10-19 11:05:32 +0700

    a

--- app.js
+++
@@ -1,6 +0,0 @@
-Stream = require('node-rtsp-stream');
-stream = new Stream({
-  name: 'name',
-  streamUrl: 'rtsp://admin:admin1234@ngduchanh2.ddns.net:554/cam/realmonitor?channel=2&subtype=0',
-  wsPort: 9999
-})
